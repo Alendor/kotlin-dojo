ee162f6b6b38e41f727731e3b43c78600f83d35d
Andrés Pino Vallejo <andrespinovallejo@gmail.com>
2018-09-16 12:53:54 -0500

    The chuck joke activity is opening from the main activity

--- app/src/main/java/com/compumovil/kotlin_dojo/Activities/MainActivity.kt
+++ app/src/main/java/com/compumovil/kotlin_dojo/Activities/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.compumovil.kotlin_dojo.Activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.compumovil.kotlin_dojo.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startBtn.setOnClickListener {
+            startActivity(Intent(this@MainActivity, ChuckJoke::class.java))
+        }
     }
 }
