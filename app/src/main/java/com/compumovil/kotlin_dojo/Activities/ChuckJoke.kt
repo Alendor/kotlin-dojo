395c241959a7de66ada35692624ac04a52a047de
Andrés Pino Vallejo <andrespinovallejo@gmail.com>
2018-09-16 12:39:24 -0500

    Base for start the dojo

---
+++ app/src/main/java/com/compumovil/kotlin_dojo/Activities/ChuckJoke.kt
@@ -0,0 +1,13 @@
+package com.compumovil.kotlin_dojo.Activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.compumovil.kotlin_dojo.R
+
+class ChuckJoke : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chuck_joke)
+    }
+}
