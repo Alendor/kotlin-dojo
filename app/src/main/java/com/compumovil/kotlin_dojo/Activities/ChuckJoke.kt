9ef1c226ba8c711b869418ac66f113978e21c37a
Andrés Pino Vallejo <andrespinovallejo@gmail.com>
2018-09-16 13:59:52 -0500

    Consuming the service without parsing the json

--- app/src/main/java/com/compumovil/kotlin_dojo/Activities/ChuckJoke.kt
+++ app/src/main/java/com/compumovil/kotlin_dojo/Activities/ChuckJoke.kt
@@ -2,12 +2,40 @@ package com.compumovil.kotlin_dojo.Activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.compumovil.kotlin_dojo.R
+import kotlinx.android.synthetic.main.activity_chuck_joke.*
+import okhttp3.*
+import java.io.IOException
 
 class ChuckJoke : AppCompatActivity() {
+
+    val client = OkHttpClient()
+    val serviceUrl = "https://api.chucknorris.io/jokes/random"
+    val request = Request.Builder().url(serviceUrl).build()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chuck_joke)
+
+        runService()
+        changeJokeBtn.setOnClickListener { runService() }
+    }
+
+    fun runService() {
+
+        client.newCall(request).enqueue(object: Callback {
+            override fun onResponse(call: Call?, response: Response?) {
+                val body = response?.body()?.string()
+                println("response")
+                println(body)
+                Toast.makeText(this@ChuckJoke, body, Toast.LENGTH_LONG).show()
+            }
+
+            override fun onFailure(call: Call?, e: IOException?) {
+                Toast.makeText(this@ChuckJoke, "Falló la petición", Toast.LENGTH_LONG).show()
+                e?.printStackTrace()
+            }
+        })
     }
 }
